000a6fe020828cf14e793f326fa83a40f0fdedda
Sid Kumar <emperorsid920@gmail.com>
2022-06-08 00:02:21 -0500

    Push all these files

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,5 +1,7 @@
 #include "expressions.h"
 
+#include "hwexpressions.h"
+
 //example
 int add_numbers(int num1, int num2)
 {
@@ -10,4 +12,16 @@ int add_numbers(int num1, int num2)
 
 
 
+double get_sales_tax_amount(double meal_amount)
+{
+    double total_amount1 = meal_amount * tax_rate;
+    
+    return total_amount1;
+}
 
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+    double totalAmount = meal_amount + tip_rate;
+    
+    return totalAmount;
+}
