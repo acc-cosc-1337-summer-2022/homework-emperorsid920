9082292a2373a300aaeb325effa76c8ef0a055d7
emperorsid920 <66718954+emperorsid920@users.noreply.github.com>
2022-06-18 22:20:17 -0500

    Update decisions.h

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,8 +1,10 @@
 //write include required statements
 
-
+#include <string>
+using namespace std;
 //Write a function(s) prototype  here
 
 
+int get_grade_points(string);
 
-
+double calculate_gpa(int,double);
