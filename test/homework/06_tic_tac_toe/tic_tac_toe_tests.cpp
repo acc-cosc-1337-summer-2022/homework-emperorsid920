6d7a372b0ebd1fd819d3869238ea4958640ef9a7
emperorsid920 <66718954+emperorsid920@users.noreply.github.com>
2022-07-20 23:58:43 -0500

    Update tic_tac_toe_tests.cpp

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,7 +1,34 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "tic_tac_toe.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+
+TEST_CASE("Verify Test Full Board") {
+    
+    Tic_Tac_Toe Game;
+    
+    Game.mark_board(1);
+    REQUIRE(Game. game_over() ==false);
+    Game.mark_board(2);
+    REQUIRE(Game. game_over() ==false);
+    Game.mark_board(3);
+    REQUIRE(Game. game_over() ==false);
+    Game.mark_board(4);
+    REQUIRE(Game. game_over() ==false);
+    Game.mark_board(5);
+    REQUIRE(Game. game_over() ==false);
+    Game.mark_board(6);
+    REQUIRE(Game. game_over() ==false);
+    Game.mark_board(7);
+    REQUIRE(Game. game_over() ==false);
+    Game.mark_board(8);
+    REQUIRE(Game. game_over() ==false);
+    Game.mark_board(9);
+    REQUIRE(Game. game_over() ==false);
+    
+    REQUIRE(Game. game_over() == true);
+}
