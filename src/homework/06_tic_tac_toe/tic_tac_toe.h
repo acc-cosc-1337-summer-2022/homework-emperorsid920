f0f20e06fa41eb8839cbdf7e9b0a292ef3308bb4
emperorsid920 <66718954+emperorsid920@users.noreply.github.com>
2022-07-20 23:52:00 -0500

    Update tic_tac_toe.h

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,34 @@
 //h
+
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+ 
+#include<iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Tic_Tac_Toe {
+private:
+    string Player;
+    
+  
+    
+    vector<string> pegs = vector<string> (9, " ");
+    
+    
+    void set_next_player();
+    bool check_board_full();
+    
+    
+public:
+    bool game_over();
+    void mark_board(int);
+    string get_player() const{return Player;}
+    void start_game(string);
+    void display_board() const;
+    void clear_board();
+}; 
+
+#endif
